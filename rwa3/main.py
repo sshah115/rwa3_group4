f645e4562c46af417c9c472ea59474fbd72bc28d
sshah115 <shailshah489@gmail.com>
2024-04-08 23:25:57 -0400

    #2 Player class draft 1 for movement

--- rwa3/main.py
+++ rwa3/main.py
@@ -1,8 +1,7 @@
 import sys
 import os.path
-import shutil
-import rpg.item as item
 from rpg.maze import Maze  # noqa: E402
+from rpg.player import Player
 
 # Locate the config.yaml file
 folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -10,23 +9,8 @@ sys.path.append(folder)
 file_path = os.path.join(folder, "rwa3", "rpg", "config.yaml")
 
 if __name__ == "__main__":
-    
-    # Create backup of original config.yaml prior to modifying it during the game
-    shutil.copy(file_path,file_path + ".original")
 
     # Print the maze
     maze = Maze(file_path)
     maze.print_maze()
-    
-    # Import the items in the maze 
-    gems, keys, padlocks, arrows, hearts = item.make_items(maze)
-    # Examples of accessing details about an item object
-    print("\nExample of accessing Item attributes: ")
-    print("Heart Item Object: ",hearts)
-    print("Heart Item name: ",hearts.item_type.name)
-    print("Heart Item position: ",hearts.item_position)
-    print("Heart Item value: ",hearts.item_value)
-    print("Heart Item emoji: ",hearts.item_emoji)
-    
-    # when done the game, replace the modified config.yaml with the original copy
-    shutil.move(file_path + ".original", file_path)
+    Player(file_path, maze)
--- rwa3/rpg/player.py
+++ rwa3/rpg/player.py
@@ -11,18 +11,28 @@ Email   : sshah115@umd.edu
 
 # Importing reqired modules
 import yaml
-from rpg.item import Item
+# from rpg.item import Item
+import rpg.item as item
+from rpg.maze import Maze  # noqa: E402
+from enum import Enum, auto
 # folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 # sys.path.append(folder)
 # file_path = os.path.join(folder, "rpg", "config.yaml")
 
+class Direction(Enum):
+    
+    up = auto()
+    down = auto()
+    left = auto()
+    right = auto()
+    
 
 class Player:
     """
     _summary_
     """
 
-    def __init__(self, config_file) -> None:
+    def __init__(self, config_file, maze) -> None:
         """
         _summary_
 
@@ -36,11 +46,17 @@ class Player:
         self._health = None
         self._inventory = {}
         self._position = None
-        self._direction = None
+        self._direction: Direction
         self._attack_power = None
+        self._maze = maze
+        self._emoji = {}
 
         self.load_config()
         self.start()
+    
+    @property    
+    def maze(self):
+        return self._maze
 
     def load_config(self):
         """
@@ -53,8 +69,11 @@ class Player:
                 self._name = player_data["name"]
                 self._health = player_data["health"]
                 self._position = player_data["position"]
-                self._direction = player_data["direction"]
+                direction_str = player_data["direction"]
+                self._direction = Direction[direction_str]
                 self._attack_power = player_data["attack_power"]
+                self._emoji = {"up": player_data["emoji_up"],"down": player_data["emoji_down"],
+                "left": player_data["emoji_left"], "right": player_data["emoji_right"]}
 
             except yaml.YAMLError as e:
                 print(f"Error parsing YAML file: {e}")
@@ -69,6 +88,10 @@ class Player:
         """
         _summary_
         """
+        # maze = Maze(self._config)
+        gems, keys, padlocks, arrows, hearts = item.make_items(self.maze)
+        # print("GEMS:\n", gems, "\nKEYS:\n", keys, "\nPADLOCKS:\n", padlocks, "\nARROWS:\n", arrows, "\nHEARTS:\n", hearts) 
+        
         print("*"*34 + "\n*** Welcome to the Maze Game! ***")
         while True:
             print("*"*34 + "\nw - move forward \
@@ -85,7 +108,14 @@ class Player:
             if action == "p":
                 print(f"🤴 Arthur has {self._health} health.")
             elif action == "i":
-                print()
+                print("*"*45 + f"\nArthur's inventory: {keys.item_emoji} x 0, {arrows.item_emoji} x 0, {gems.item_emoji} x 0\n" + "*"*45)
+                self.maze.print_maze()
+            elif action in  ('w', 's', 'd', 'a'):
+                self.move(action)
+                self.maze.print_maze()
+            else:
+                print(f"Invalid command, please try again.")
+                
             # # To continue the prompt if any other character entered
             # elif action not in ["w", "s", "a", "d"]:
             #     continue
@@ -101,11 +131,47 @@ class Player:
         """
         _summary_
         """
-        pass
-        # if action == "w":
-        #     if self._direction == "up":
-                
-                
+        if action == "w":
+            if self._direction == Direction.up:
+                self.maze._grid[self.maze._player_position[0]][self.maze._player_position[1]] = "  "
+                self.maze._player_position = (self.maze._player_position[0]-1, self.maze._player_position[1])
+                self.maze.spawn_player()
+            elif self._direction == Direction.left:
+                self.maze._grid[self.maze._player_position[0]][self.maze._player_position[1]] = "  "
+                self.maze._player_position = (self.maze._player_position[0], self.maze._player_position[1]-1)
+                self.maze.spawn_player()
+            elif self._direction == Direction.down:
+                self.maze._grid[self.maze._player_position[0]][self.maze._player_position[1]] = "  "
+                self.maze._player_position = (self.maze._player_position[0]+1, self.maze._player_position[1])
+                self.maze.spawn_player()
+            else:
+                self.maze._grid[self.maze._player_position[0]][self.maze._player_position[1]] = "  "
+                self.maze._player_position = (self.maze._player_position[0], self.maze._player_position[1]+1)
+                self.maze.spawn_player()
+        if action == "s":
+            if self._direction == Direction.up:
+                self.maze._grid[self.maze._player_position[0]][self.maze._player_position[1]] = "  "
+                self.maze._player_position = (self.maze._player_position[0]+1, self.maze._player_position[1])
+                self.maze.spawn_player()
+            elif self._direction == Direction.left:
+                self.maze._grid[self.maze._player_position[0]][self.maze._player_position[1]] = "  "
+                self.maze._player_position = (self.maze._player_position[0], self.maze._player_position[1]+1)
+                self.maze.spawn_player()
+            elif self._direction == Direction.down:
+                self.maze._grid[self.maze._player_position[0]][self.maze._player_position[1]] = "  "
+                self.maze._player_position = (self.maze._player_position[0]-1, self.maze._player_position[1])
+                self.maze.spawn_player()
+            else:
+                self.maze._grid[self.maze._player_position[0]][self.maze._player_position[1]] = "  "
+                self.maze._player_position = (self.maze._player_position[0], self.maze._player_position[1]-1)
+                self.maze.spawn_player()                                   
+        elif action == "a":
+            self.rotate("left")
+            self.maze.spawn_player()
+        elif action == "d":
+            self.rotate("right")
+            self.maze.spawn_player()
+            
                 
         # elif action == "s":
         #     move_backward()
@@ -119,38 +185,41 @@ class Player:
         # # In case user inputs invalid action keyword.
         # else:
         #     print("Invalid action! Please enter a valid action")
+        
+    def rotate(self, direction):
+        if direction == "left":
+            if self._direction == Direction.up:
+                self._direction = Direction.left
+                # print(self._emoji["left"])
+                self.maze._player_emoji = self._emoji["left"]
+                
+            elif self._direction == Direction.left:
+                self._direction = Direction.down
+                self.maze._player_emoji = self._emoji["down"]
+                
+            elif self._direction == Direction.down:
+                self._direction = Direction.right
+                self.maze._player_emoji = self._emoji["right"]
+                
+            elif self._direction == Direction.right:
+                self._direction = Direction.up
+                self.maze._player_emoji = self._emoji["up"]
+                
+        elif direction == "right":  
+            if self._direction == Direction.up:
+                self._direction = Direction.right
+                self.maze._player_emoji = self._emoji["right"]
+                
+            elif self._direction == Direction.right:
+                self._direction = Direction.down
+                self.maze._player_emoji = self._emoji["down"]
+                
+            elif self._direction == Direction.down:
+                self._direction = Direction.left
+                self.maze._player_emoji = self._emoji["left"]
+                
+            elif self._direction == Direction.left:
+                self._direction = Direction.up
+                self.maze._player_emoji = self._emoji["up"]            
 
-    def attack(self, enemy, damage):
-        """
-        _summary_
 
-        Args:
-            enemy (_type_): _description_
-            damage (_type_): _description_
-        """
-        print(f"{self.name} attacks {enemy.name}!")
-        enemy.take_damage(damage)
-
-    def defend(self):
-        """
-        _summary_
-        """
-        print(f"{self.name} defends!")
-
-    def take_damage(self, damage):
-        """
-        _summary_
-
-        Args:
-            damage (_type_): _description_
-        """
-        self.health -= damage
-        if self.health <= 0:
-            print(f"{self.name} has been defeated")
-        else:
-            print(f"{self.name} has {self.health} health left")
-
-
-# if __name__ == "__main__":
-#     # For testing
-#     arthur = Player(file_path)
